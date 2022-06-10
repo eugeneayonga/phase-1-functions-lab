b953b264907be9746e901ea4e556a7f74e80e0d7
Eugene <eugeneayonga@gmail.com>
2022-06-10 19:32:51 +0300

    commit tree 99a5d220fe3a14a6130541ce8b20954f58d38fae

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+// FUNCTION ONE
+function distanceFromHqInBlocks(location) {
+    //returns the number of blocks given a value
+    if (location < 42) {
+        return (42 - location);
+    } else {
+        return (location - 42);
+    }
+}
+
+console.log(distanceFromHqInBlocks(43));
+
+
+// FUNCTION TWO
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264;
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+
+}
+
+console.log(distanceFromHqInFeet(43));
+
+
+// FUNCTION THREE
+function distanceTravelledInFeet(pointZero, pointOne) {
+    //returns the number of feet traveled
+    if (pointOne > pointZero) {
+        return ((pointOne - pointZero) * 264);
+    } else {
+        return ((pointZero - pointOne) * 264);
+    }
+}
+
+console.log(distanceTravelledInFeet(0, 8));
+
+
+// FUNCTION FOUR
+function calculatesFarePrice(pointZero, pointOne) {
+    //returns the fare for the customer
+    const distanceTravelled = distanceTravelledInFeet(pointZero, pointOne);
+
+    if (distanceTravelled <= 400) {
+        return 0;
+    } if (distanceTravelled >= 400 && distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * 0.02;
+    } else if (distanceTravelled >= 2000 && distanceTravelled <= 2500) {
+        return 25;
+    } else if (distanceTravelled > 2500) {
+        return "cannot travel that far"
+    }
+}
